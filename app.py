cc284d135efd3630034a533ea0e32426f8942dfd
yuyunwuuu <lori78216@gmail.com>
2024-05-29 21:16:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ line_bot_api = LineBotApi('XRQ8El1uzN6JCD41zVpWWmIFd/5sEfb0w829GB4rj39cakj1l0WE7
 # Channel Secret
 handler = WebhookHandler('e644248b4840d4dd112e2faee5c56c79')
 # OPENAI API Key初始化設定
-openai.api_key = ('sk-proj-NoigWu1g89WWA2Fa8gBkT3BlbkFJp4l25YFnMUVf7odSj2PA')
+openai.api_key = ('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')
 
 
 def GPT_response(text):
